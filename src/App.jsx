2e567d34bc3a1b36ea24c4cb1d7bf674c177d508
Swagnik Das <104345028+swagthehooman@users.noreply.github.com>
2023-05-27 08:40:14 +0530

    project completion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./app.css";
 import DateDisplay from "./components/dateDisplay/DateDisplay";
 import DateInput from "./components/dateInput/DateInput";
@@ -10,63 +10,118 @@ export default function App() {
 
   const currentDate = new Date().getTime();
 
+  //states for calculated date difference
   const [day, setDay] = useState("");
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
 
+  //states for control of input errors
   const [dayError, setDayError] = useState(false);
   const [monthError, setMonthError] = useState(false);
   const [yearError, setYearError] = useState(false);
 
+  //state for display controls
   const [toDisplay, setToDisplay] = useState(false);
 
-  // handle changes
+  // handle changes in input form
   function handleInput(event) {
     var value = event.target.value;
     var name = event.target.name;
-    if (name === "dayInput") {
-      if (value.length > 2 || value.length === 0) setDayError(true);
-      setDayInput(value);
-    } else if (name === "monthInput") {
-      if (value.length > 2 || value.length === 0) setMonthError(true);
-      setMonthInput(value);
-    } else {
-      if (value.length > 4 || value.length === 0) setYearError(true);
-      setYearInput(value);
-    }
+    if (name === "dayInput") setDayInput(value);
+    else if (name === "monthInput") setMonthInput(value);
+    else setYearInput(value);
   }
 
   //calculate date difference
   function calculateDiff(event) {
-    console.log("click event");
-    if (dayInput.length === 0) setDayError(true);
-    else setDayError(false);
-    if (monthInput.length === 0) setMonthError(true);
-    else setMonthError(false);
-    if (yearInput.length === 0) setYearError(true);
+    setYearError(false);
+    setMonthError(false);
+    setDayError(false);
+    //check if inputs are empty or not
+    if (yearInput === "") setYearError(true);
+    if (monthInput === "") setMonthError(true);
+    if (dayInput === "") setDayError(true);
+
+    var tempDay = parseInt(dayInput);
+    var tempMonth = parseInt(monthInput);
+    var tempYear = parseInt(yearInput);
+    //check month validity
+    if (tempMonth >= 1 && tempMonth <= 12) setMonthError(false);
+    else setMonthError(true);
+
+    //check year validity
+    if (yearInput.length > 4 || yearInput > new Date().getFullYear())
+      setYearError(true);
     else setYearError(false);
 
-    if (
-      dayInput.length === 0 ||
-      monthInput.length === 0 ||
-      yearInput.length === 0
-    ) {
-      //if there is any error, don't calculate
-
-      return;
+    //check day validity
+    if ((tempYear % 4 === 0 && tempYear % 100 !== 0) || tempYear % 400 === 0) {
+      //leap year
+      if (tempMonth === 2) {
+        //for feb
+        if (tempDay >= 1 && tempDay <= 29)
+          setDayError(false); //if days between 1 and 29
+        else setDayError(true);
+      } else if (
+        //for jan,march, may, jul, aug, oct and dec
+        tempMonth === 1 ||
+        tempMonth === 3 ||
+        tempMonth === 5 ||
+        tempMonth === 7 ||
+        tempMonth === 8 ||
+        tempMonth === 10 ||
+        tempMonth === 12
+      ) {
+        if (tempDay >= 1 && tempDay <= 31)
+          setDayError(false); //if days between 1 and 31
+        else setDayError(true);
+      } else {
+        //for rest of the month
+        if (tempDay >= 1 && tempDay <= 30)
+          setDayError(false); //if days between 1 and 30
+        else setDayError(true);
+      }
     } else {
+      //not a leap year
+      if (tempMonth === 2) {
+        if (tempDay >= 1 && tempDay <= 28) setDayError(false);
+        else setDayError(true);
+      } else if (
+        tempMonth === 1 ||
+        tempMonth === 3 ||
+        tempMonth === 5 ||
+        tempMonth === 7 ||
+        tempMonth === 8 ||
+        tempMonth === 10 ||
+        tempMonth === 12
+      ) {
+        if (tempDay >= 1 && tempDay <= 31) setDayError(false);
+        else setDayError(true);
+      } else {
+        if (tempDay >= 1 && tempDay <= 30) setDayError(false);
+        else setDayError(true);
+      }
+    }
+
+    if (dayError || monthError || yearError) {
+      //if error exists, dont display
+      setToDisplay(false);
+      return;
+    } //if has error, exit
+    else {
       const newDate = new Date(yearInput, monthInput, dayInput);
       var diffInDate = new Date(currentDate - newDate.getTime());
       if (diffInDate < 0) {
-        setYearError(true);
+        setToDisplay(false); //if the year is in future, return
         return;
-      }
-      setYear(diffInDate.getUTCFullYear() - 1970);
-      setMonth(diffInDate.getMonth());
-      setDay(diffInDate.getUTCDate());
+      } else {
+        setYear(diffInDate.getUTCFullYear() - 1970);
+        setMonth(diffInDate.getMonth());
+        setDay(diffInDate.getUTCDate());
 
-      setToDisplay(true); //display final difference
-    }
+        setToDisplay(true);
+      }
+    } //display final difference
   }
   return (
     <main>
@@ -80,7 +135,7 @@ export default function App() {
           month={monthInput}
           year={yearInput}
         />
-        <div className="middle_div">
+        <div className="calculation_div">
           <hr />
           <button onClick={calculateDiff}>
             <img src="./assets/images/icon-arrow.svg" />
--- src/components/dateInput/DateInput.jsx
+++ src/components/dateInput/DateInput.jsx
@@ -4,7 +4,14 @@ export default function DateInput(props) {
   return (
     <div className="date_input">
       <div className="input_field">
-        <label style={{ color: props.dayError && "red" }}>DAY</label>
+        <label
+          style={{
+            color:
+              (props.monthError || props.dayError || props.yearError) && "red",
+          }}
+        >
+          DAY
+        </label>
         <input
           type="text"
           placeholder="DD"
@@ -12,10 +19,22 @@ export default function DateInput(props) {
           value={props.day}
           onChange={props.handleChange}
         />
-        {props.dayError && <p className="errorInput">This field is required</p>}
+        {props.dayError && props.day === "" && (
+          <p className="errorInput">This field is required</p>
+        )}
+        {props.dayError && props.day !== "" && (
+          <p className="errorInput">Mus be a valid day</p>
+        )}
       </div>
       <div className="input_field">
-        <label style={{ color: props.monthError && "red" }}>MONTH</label>
+        <label
+          style={{
+            color:
+              (props.monthError || props.dayError || props.yearError) && "red",
+          }}
+        >
+          MONTH
+        </label>
         <input
           type="text"
           placeholder="MM"
@@ -23,12 +42,22 @@ export default function DateInput(props) {
           value={props.month}
           onChange={props.handleChange}
         />
-        {props.monthError && (
+        {props.monthError && props.month === "" && (
           <p className="errorInput">This field is required</p>
+        )}
+        {props.monthError && props.month !== "" && (
+          <p className="errorInput">Must be a valid month</p>
         )}
       </div>
       <div className="input_field">
-        <label style={{ color: props.yearError && "red" }}>YEAR</label>
+        <label
+          style={{
+            color:
+              (props.monthError || props.dayError || props.yearError) && "red",
+          }}
+        >
+          YEAR
+        </label>
         <input
           type="text"
           placeholder="YYYY"
@@ -36,8 +65,11 @@ export default function DateInput(props) {
           value={props.year}
           onChange={props.handleChange}
         />
-        {props.yearError && (
+        {props.yearError && props.year === "" && (
           <p className="errorInput">This field is required</p>
+        )}
+        {props.yearError && props.year !== "" && (
+          <p className="errorInput">Must be in the past</p>
         )}
       </div>
     </div>
